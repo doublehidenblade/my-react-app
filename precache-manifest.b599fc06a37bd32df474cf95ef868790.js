6ef57e3c13fbb4192fc7df3e28995a7209fc2e4a
Dingyu Peng <dpeng4@illinois.edu>
2019-11-28 18:45:38 -0600

    Updates

---
+++ precache-manifest.b599fc06a37bd32df474cf95ef868790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c665c9a10c95a5da5cbf6985b752c61",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "4d8d9cdb20c886cdcc78",
+    "url": "/my-react-app/static/css/main.8927bdaa.chunk.css"
+  },
+  {
+    "revision": "a0ba22936bb3e99c591a",
+    "url": "/my-react-app/static/js/2.b770d6c5.chunk.js"
+  },
+  {
+    "revision": "4d8d9cdb20c886cdcc78",
+    "url": "/my-react-app/static/js/main.c810f510.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  }
+]);
